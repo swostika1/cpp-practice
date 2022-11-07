6fc92b2efdfe9306b15414dcc7f420f2f8b53731
Swostika <poudelswostika2@gmail.com>
2022-11-07 21:33:17 +0545

    Corrected access specifiers code

--- Day1.cpp
+++ Day1.cpp
@@ -5,6 +5,21 @@ class book
     char  name[20];
     int price:
     public:
-    void getdata(char name, int a);
-    void showdata();
+    void getdata()
+     {
+    	cout<<"Enter the name of the book";
+    	cin>>name;
+    	cout<<"Enter the price of the book";
+    	cin>>price;
+	}
+    void showdata(){
+    cout<<"The name of the book is"<<name<<"and price is"<<price<<endl;
+    }
 };
+int main()
+{
+    Book b1;
+    b1.getdata();
+    b1.showdata();
+    return 0;
+}
