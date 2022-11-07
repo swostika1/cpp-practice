c4ab688ab9c0e2fb566071855a4685b44bf6a4d8
Swostika <poudelswostika2@gmail.com>
2022-11-07 20:48:01 +0545

    Added another message

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 auto main(int, char**, char**) -> int {
     std::cout << "Hello, world" << std::endl;
+    std::cout << "Hello, world again" << std::endl;
 
     std::cout << std::endl;
     return 0;
